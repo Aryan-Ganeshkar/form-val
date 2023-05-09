8a69e31ea62637c37e02b943dd36a10e50c8e1ff
Aryangit@9284 <ganeshkararyan@gmail.com>
2023-05-09 14:22:39 +0530

    components addeds

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,9 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Formval from './components/Formval';
 function App() {
 
-  const submitFrom = () => {
-
-    console.log('name', fname);
-    console.log('email' , femail);
-    console.log('num' , mnum);
-    console.log('password', pass)
-  }
-
-  const [fname, setFname] = useState('');
-
-  const [femail , setFemail] = useState('');
-
-  const [mnum , setMnum] = useState('');
-
-  const [pass , setPass] = useState('')
-
-
   return (
-    <div className="App">
-      <div>
-        <p> Registation From </p>
-      </div>
-      <div>
-        <input type = 'text' value={fname} onChange={(e) =>setFname(e.target.value)} placeholder='Enter your name'/>
-      </div>
-      <br />
-      <div>
-        <input type = 'text' value={mnum} onChange={(e) =>setMnum(e.target.value)} placeholder='Enter your mobile number' />
-      </div>
-      <br />
-      <div>
-        <input type = 'email' value={femail} onChange={(e) =>setFemail(e.target.value)} placeholder='Enter your email'/>
-      </div>
-      <br />
-      <div>
-        <input type = 'password' value={pass} onChange={(e) =>setPass(e.target.value)} placeholder='Enter your password' /> 
-      </div>
-      <br />
-      <div>
-        <button onClick={ () => {submitFrom()}}> submit </button>
-      </div>
-
-    </div>
+        <Formval />
   );
 }
 
---
+++ src/components/Formval.tsx
@@ -0,0 +1,75 @@
+import { useState } from "react"
+import './Formval.css';
+const Formval = () => {
+
+    const education = ['bca' , 'mca' , 'bba' , 'be' , 'ba']
+
+  const returnmap = education.map((setData) => {
+    return(
+      <option>
+          {setData}
+      </option>
+    )
+  })
+
+  const submitFrom = () => {
+
+    console.log('name', fname);
+    console.log('email' , femail);
+    console.log('num' , mnum);
+    console.log('password', pass);
+    
+  }
+
+  const [fname, setFname] = useState('');
+
+  const [femail , setFemail] = useState('');
+
+  const [mnum , setMnum] = useState('');
+
+  const [pass , setPass] = useState('');
+
+  const [data , setData] = useState([]);
+
+    return(
+        <div className="App">
+        <div className='App1'>
+          <p> Registation From </p>
+        </div>
+        <div>
+        <label htmlFor='name'> Username </label>
+        <br />
+          <input type = 'text' value={fname} onChange={(e) =>setFname(e.target.value)} placeholder='Enter your name'/>
+        </div>
+        <br />
+        <div>
+        <label htmlFor='name'> Mobile no</label>
+        <br />
+          <input type = 'text' value={mnum} onChange={(e) =>setMnum(e.target.value)} placeholder='Enter your mobile number' />
+        </div>
+        <br />
+        <div>
+        <label htmlFor='name'> Email id </label>
+        <br />
+          <input type = 'email' value={femail} onChange={(e) =>setFemail(e.target.value)} placeholder='Enter your email'/>
+        </div>
+        <br />
+        <div>
+        <label htmlFor='name'> Password </label>
+        <br />
+          <input type = 'password' value={pass} onChange={(e) =>setPass(e.target.value)} placeholder='Enter your password' /> 
+        </div>
+        <br />
+        <div className='App3'>
+          <select>
+            {returnmap}
+          </select>
+        </div>
+        <br />
+          <button onClick={ () => {submitFrom()}}> submit </button>
+       
+     </div>
+    )
+}
+
+export default Formval;
